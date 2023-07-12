0d34f32efe1df76a464b8d631292f82a44f0a595
conio11 <conio416@naver.com>
2023-07-12 16:53:42 +0900

    Filter, Listener 사용을 위한 @ServletComponentScan 추가

--- mvcboard/src/main/java/com/goodee/mvcboard/MvcboardApplication.java
+++ mvcboard/src/main/java/com/goodee/mvcboard/MvcboardApplication.java
@@ -2,8 +2,10 @@ package com.goodee.mvcboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // @ 추가
 public class MvcboardApplication {
 
 	public static void main(String[] args) {

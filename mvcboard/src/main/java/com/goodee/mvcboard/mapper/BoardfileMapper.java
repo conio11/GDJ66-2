4505cc74a78937513450351ae0bccd462ff26419
conio11 <conio416@naver.com>
2023-07-15 22:36:26 +0900

    Revert "fileupload 수정, 삭제 Mapper"
    This reverts commit 521552a3c2bb7af9138d74a949e1c8a9f08372fa.

--- mvcboard/src/main/java/com/goodee/mvcboard/mapper/BoardfileMapper.java
+++ mvcboard/src/main/java/com/goodee/mvcboard/mapper/BoardfileMapper.java
@@ -4,25 +4,18 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.goodee.mvcboard.vo.Board;
 import com.goodee.mvcboard.vo.Boardfile;
 
 @Mapper
 public interface BoardfileMapper {
-	// 파일 업로드
 	int insertBoardfile(Boardfile boardfile);
 	
 	// 업로드 파일 상세정보 - 한 게시글에 여러 파일의 정보 담길 수 있으므로 List
-	List<Boardfile> selectBoardfile(int boardNo);
+	List<Boardfile> selectBoardfileOne(int boardNo);
+	
+	// 파일 수정
 	
 	
-	// 게시글 내 모든 파일 삭제
-	int removeBoardfile(int boardNo);
-	
-	// 게시글 내 파일 삭제 (1개인 경우)
-	int removeBoardfileOne(int boardfileNo);
-	
-	// 게시글 내 첨부파일 수
-	int selectBoardfileCnt(int boardNo);
+	// 파일 삭제
 	
 }

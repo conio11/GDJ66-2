5e9ad3627ae9ce083ff09f59ac5cd3cabd83b279
conio11 <conio416@naver.com>
2023-08-16 18:08:32 +0900

    로그인 mapper 수정

--- myJoinTree/src/main/java/com/goodee/myJoinTree/mapper/LoginMapper.java
+++ myJoinTree/src/main/java/com/goodee/myJoinTree/mapper/LoginMapper.java
@@ -3,9 +3,19 @@ package com.goodee.myJoinTree.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.goodee.myJoinTree.vo.AccountList;
+import com.goodee.myJoinTree.vo.EmpInfo;
 
 @Mapper
 public interface LoginMapper {
 	// 로그인 액션
 	AccountList selectAccount(AccountList account);
+	
+	// 로그인 성공 시 empNo로 empName 출력
+	String selectEmpName(int empNo);
+	
+	// 사번, 주민등록번호 뒷자리 체크
+	int selectEmpNoJumin(int empNo, String juminNo);
+	
+	// 비밀번호 재설정 (분실)
+	int modifyForgetPw(int empNo, String newPw);
 }

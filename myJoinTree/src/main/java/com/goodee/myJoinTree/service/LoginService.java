e3130112fe4d8e97bf13c095468c965fb30265bc
conio11 <conio416@naver.com>
2023-08-16 18:06:53 +0900

    로그인 이후 empName 출력, 비밀번호 분실 시 실행 메소드

--- myJoinTree/src/main/java/com/goodee/myJoinTree/service/LoginService.java
+++ myJoinTree/src/main/java/com/goodee/myJoinTree/service/LoginService.java
@@ -13,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Transactional
 public class LoginService {
+	static final String CYAN = "\u001B[46m";
+	static final String RESET = "\u001B[0m";
+	
 	@Autowired
 	private LoginMapper loginMapper;
 	
@@ -21,4 +24,26 @@ public class LoginService {
 		AccountList accountEmp = loginMapper.selectAccount(account);
 		return accountEmp;
 	}
+	
+	// 로그인 성공 시 empNo로 empName 출력
+	public String getEmpName(int empNo) {
+		return loginMapper.selectEmpName(empNo);
+	}
+	
+	// 사원, 주민번호 뒷자리 일치 체크 (비밀번호 분실 시)
+	public int selectEmpNoJumin(int empNo, String juminNo) {
+		int row = loginMapper.selectEmpNoJumin(empNo, juminNo);
+		log.debug(CYAN + row + " <-- row(LoginService-selectEmpNoJumin)" + RESET);
+		
+		return row;
+	}
+	
+	// 비밀번호 변경 (비밀번호 분실 시)
+	public int modifyForgetPw(int empNo, String newPw) {
+		int row = loginMapper.modifyForgetPw(empNo, newPw);
+		log.debug(CYAN + row + " <-- row(LoginService-modifyForgetPw)" + RESET);
+		
+		return row;
+	}
+	
 }
